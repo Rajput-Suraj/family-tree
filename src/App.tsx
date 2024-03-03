fd09c1f267015bdbe04c996cbc6eb8f5609b932d
Rajput-Suraj <surajarajput@gmail.com>
2024-03-03 11:01:34 +0530

    Basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
+import { useCallback } from 'react';
+import ReactFlow, {
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+} from 'reactflow';
+
+import 'reactflow/dist/style.css';
+
+type Params = {
+  source: string | null,
+  sourceHandle: string | null,
+  target: string | null,
+  targetHandle: string | null
+}
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
 function App() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: Params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
 
   return (
-    <>
-
-    </>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onConnect={onConnect}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+      >
+        <Controls />
+        <MiniMap />
+        <Background gap={12} size={1} />
+      </ReactFlow>
+    </div>
   )
 }
 
