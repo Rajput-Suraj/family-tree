8adf07e2ff67afd1864e57effda7614ce390fed0
Rajput-Suraj <surajarajput@gmail.com>
2024-03-06 14:03:13 +0530

    Add sibling node

--- src/constant.ts
+++ src/constant.ts
@@ -2,21 +2,16 @@ import { Node, Edge } from 'reactflow';
 
 export const initialNodes: Node[] = [
   { id: 'node-1', type: 'customNode', position: { x: 165, y: 0 }, data: {} },
-  {
-    id: 'node-2',
-    type: 'output',
-    position: { x: 0, y: 200 },
-    data: { label: 'node 2' },
-  },
+  { id: 'node-2', type: 'customNode', position: { x: 265, y: 0 }, data: {} },
   {
     id: 'node-3',
     type: 'output',
     position: { x: 200, y: 200 },
-    data: { label: 'node 3' },
+    data: { label: 'Child' },
   },
 ];
 
 export const initialEdges: Edge[] = [
-  { id: 'edge-1', source: 'node-1', target: 'node-2', sourceHandle: 'a' },
-  { id: 'edge-2', source: 'node-1', target: 'node-3', sourceHandle: 'a' },
+  { id: 'edge-1', source: 'node-1', target: 'node-3', sourceHandle: 'a' },
+  { id: 'edge-2', source: 'node-2', target: 'node-3', sourceHandle: 'a' },
 ];
