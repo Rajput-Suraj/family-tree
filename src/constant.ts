869ff9f13b294b13fc1b49fe529bcdef2b858d2b
Rajput-Suraj <surajarajput@gmail.com>
2024-03-08 17:44:05 +0530

    Trying to connect/make graph

--- src/constant.ts
+++ src/constant.ts
@@ -1,18 +1,25 @@
 import { Node, Edge } from 'reactflow';
 
 export const initialNodes: Node[] = [
-  { id: 'node-1', type: 'customNode', position: { x: 165, y: 0 }, data: {} },
-  { id: 'node-2', type: 'customNode', position: { x: 265, y: 0 }, data: {} },
+  { id: 'node-1', type: 'customNode', position: { x: 260, y: 0 }, data: {} },
+  { id: 'node-2', type: 'customNode', position: { x: 460, y: 0 }, data: {} },
   {
     id: 'node-3',
     type: 'output',
     position: { x: 200, y: 200 },
-    data: { label: 'Child' },
+    data: { label: 'Child 1' },
+  },
+  {
+    id: 'node-4',
+    type: 'output',
+    position: { x: 400, y: 200 },
+    data: { label: 'Child 2' },
   },
 ];
 
 export const initialEdges: Edge[] = [
-  { id: 'edge-1', source: 'node-1', target: 'node-3', sourceHandle: 'a' },
-  { id: 'edge-2', source: 'node-2', target: 'node-3', sourceHandle: 'a' },
-  { id: 'edge-3', source: 'node-3', target: 'node-4', sourceHandle: 'b' },
+  { id: 'edge-1', source: 'node-1', target: 'node-3', sourceHandle: 'a', type: 'step' },
+  { id: 'edge-2', source: 'node-2', target: 'node-3', sourceHandle: 'b', type: 'step' },
+  { id: 'edge-3', source: 'node-1', target: 'node-4', sourceHandle: 'a', type: 'step' },
+  { id: 'edge-4', source: 'node-2', target: 'node-4', sourceHandle: 'b', type: 'step' },
 ];
