c8976ca8487ed26609d506f7f3acca9c23468374
Suraj Rajput <49227244+Rajput-Suraj@users.noreply.github.com>
2024-04-29 16:29:41 +0530

    Update CustomNode.tsx

--- src/components/CustomNode.tsx
+++ src/components/CustomNode.tsx
@@ -7,7 +7,7 @@ function CustomNode() {
   return (
     <div className='rounded-full border'>
       <div className="nodrag">
-        <img className="w-5 h-5" src={UserIcon} alt="User-Icon" />
+        <img className="w-5 h-5" src={UserIcon} alt="User-Icon-image" />
       </div>
       <Handle type="source" position={Position.Bottom} id="a" />
     </div>
